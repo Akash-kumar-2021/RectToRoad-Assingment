6af5d6a73da32fb3c2d2f071b8cf2c5ef93deff2
Akash kumar <78096363+Akash-kumar-2021@users.noreply.github.com>
2022-04-29 11:13:36 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,27 @@
+function navbar()
+{
+    // return `  <h3>
+    //         <a href="">Game</a>
+    //     </h3>
+    //     <div id="options">
+    //         <h3>
+    //             <a href="/about.html">About</a>
+    //         </h3>
+
+    //         <h3>
+    //             <a href="/contact.html">Contact Us</a>
+    //         </h3>
+
+    //         <h3>
+    //             <a href="login.html">Login</a>
+    //         </h3>
+    //         <h3>
+    //             <a href="singup.html">SingUp</a>
+    //         </h3> 
+    //         </div>
+    //         `;
+    return ` <img src="https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/v1591261933/NewWebsiteResource2020/Assets/Img/Logo.svg" width="6%"  alt="">
+
+       <i>Order Your Favorite Food........</i>`
+}
+export { navbar };
